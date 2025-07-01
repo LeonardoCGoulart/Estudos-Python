f8babf87aa5b678c6afcfcb6373b837fc43b62e5
LeonardoCGoulart <joedarkalone@hotmail.com>
2025-07-01 08:00:58 -0300

    exercicio 1 - contar vogais

---
+++ exercicios basicos sintaxe.py
@@ -0,0 +1,12 @@
+#EXERCICIO 1:  Manipulação de Strings
+#Contar vogais:
+frase = "Analise de dados com Python"
+# Conte quantas vogais existem na frase
+vogais = "aeiouAEIOU"
+contador_vogais = sum(1 for letra in frase
+                  if
+                   letra
+                  in
+                   vogais
+                  )
+print(f"Quantidade de vogais na frase: {contador_vogais}")
